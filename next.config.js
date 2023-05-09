22008a79a84c6370f8fc1f08abdaa68b384f682f
Enrypase <pasettoenrico02@gmail.com>
2023-05-10 00:56:02 +0200

    fix: adapted next.config.js to github pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const production = false;
+const nextConfig = !production
+  ? {
+      output: "export",
+      images: {
+        unoptimized: true,
+      },
+    }
+  : {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
