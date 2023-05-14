62adf436fe26007999107c7be799241fb5366832
Enrypase <pasettoenrico02@gmail.com>
2023-05-14 17:34:43 +0200

    fix: fixed /public images link

--- src/commonFrontend.js
+++ src/commonFrontend.js
@@ -1,5 +1,5 @@
 const GITHUB_URL =
   "https://raw.githubusercontent.com/BYS-Project/BYS_Website/main/public";
-export function translateSrc({ src }) {
+export function translateSrc(src) {
   return GITHUB_URL + src;
 }
