1a4152c193356f80c34e7cff4b3145c5b512ad18
Enrypase <pasettoenrico02@gmail.com>
2023-05-17 22:55:40 +0200

    fix: modified employees description

--- src/app/(defaultLayout)/aboutus/page.js
+++ src/app/(defaultLayout)/aboutus/page.js
@@ -23,7 +23,8 @@ export default function AboutUs() {
               name="Claudio Disma"
               surname="Brunetto Santolin"
               role="CEO"
-              description="Descr1"
+              description="I'm a creative, highly motivated person who has a passion for the Crypto industry. 
+              I've worked in the cryptocurrency business for more than 6 years. I have experience in project management, data analysis, HR, and market and financial analysis."
               imageLink={translateSrc(
                 "/employees/claudiodismabrunettosantolin.png"
               )}
@@ -37,7 +38,7 @@ export default function AboutUs() {
               name="Samuele"
               surname="Gobbo"
               role="COO"
-              description="Descr2"
+              description="Since 2020, I have specialized in the NFT market as a passionate professional. I established myself as a content creator and copywriter, delivering unique and original ideas in the industry. "
               imageLink={translateSrc("/employees/samuelegobbo.png")}
             />
           </Link>
@@ -48,8 +49,8 @@ export default function AboutUs() {
             <EmployeePic
               name="Enrico"
               surname="Pasetto"
-              role="CTO"
-              description="Descr3"
+              role="CTO/CDO"
+              description="I am a full stack developer who has a love for anything IT. I have developed expertise in a number of programming languages, including C, Java, Solidity, and JavaScript, over the course of more than four years."
               imageLink={translateSrc("/employees/enricopasetto.png")}
             />
           </Link>
@@ -61,7 +62,7 @@ export default function AboutUs() {
               name="Dylan"
               surname="Le Hir"
               role="CRO"
-              description="Descr4"
+              description="I'm an experienced professional with a background in health management and software development. My skills include proficiency in Javascript, Angular, Swift, software development, cyber security, unit testing, and conformity."
               imageLink={translateSrc("/employees/dylanlehir.png")}
             />
           </Link>
@@ -73,7 +74,7 @@ export default function AboutUs() {
               name="Jacopo"
               surname="Armini"
               role="Art Director"
-              description="Descr5"
+              description="I'm a visionary Art Director and Digital Designer, passionate about creating captivating visual stories that inspire and engage audiences. My portfolio includes innovative work for BYS Project LTD, as well as unique freelance Digital Illustration projects and assisting in comic book design"
               imageLink={translateSrc("/employees/jacopoarmini.png")}
             />
           </Link>
@@ -85,7 +86,7 @@ export default function AboutUs() {
               name="Hermes"
               surname="Carraro"
               role="Illustrator"
-              description="Descr6"
+              description='As an Illustrator, I create artwork that resonates with audiences. I have experience working at Studio Valentino Villanova in Massanzago (PD), as well as showcasing my work as an EXPO Illustrator for the "I fumetti del Dese - Mostra di Mostri" exhibition.'
               imageLink={translateSrc("/employees/hermescarraro.png")}
             />
           </Link>
