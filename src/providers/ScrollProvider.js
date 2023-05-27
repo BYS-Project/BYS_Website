17f22ab5157ea904b19363efde218357e76081a3
Enrypase <pasettoenrico02@gmail.com>
2023-05-27 18:16:33 +0200

    fix: added throttle to ScrollProvider

--- src/providers/ScrollProvider.js
+++ src/providers/ScrollProvider.js
@@ -1,16 +1,31 @@
 "use client";
 
 import ScrollContext from "@/contexts/ScrollContext";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function ScrollProvider({ children }) {
   const [scroll, setScroll] = useState();
+  const isThrottled = useRef(false);
   function handleScroll() {
     setScroll(window.scrollY);
   }
+  function throttle(fn, delay) {
+    let timeoutId;
+    return function (...args) {
+      if (isThrottled.current) {
+        return;
+      }
+      fn(...args);
+      isThrottled.current = true;
+      timeoutId = setTimeout(() => {
+        isThrottled.current = false;
+      }, delay);
+    };
+  }
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", throttle(handleScroll, 15));
+    return () =>
+      window.removeEventListener("scroll", throttle(handleScroll, 15));
   }, []);
   return (
     <ScrollContext.Provider value={{ scroll }}>
